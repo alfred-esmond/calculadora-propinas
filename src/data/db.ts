53ee525071ccb2fb78efcd0050689ec09eb0411e
alfred-esmond <alfred.esmond@gmail.com>
2024-08-28 15:55:18 +0200

    first commit

---
+++ src/data/db.ts
@@ -0,0 +1,63 @@
+import type { MenuItem } from "../types";
+export const menuItems: MenuItem[] = [
+  {
+    id: 1,
+    name: "Pizza a la Leña Pequeña",
+    price: 4.5,
+  },
+  {
+    id: 2,
+    name: "Pizza a la Leña Mediana",
+    price: 5.5,
+  },
+  {
+    id: 3,
+    name: "Rebanada de Pan con Tomate",
+    price: 1.5,
+  },
+  {
+    id: 4,
+    name: "Trozo de Pastel de Chocolate",
+    price: 1.5,
+  },
+  {
+    id: 5,
+    name: "Zumo de Naranja",
+    price: 1.7,
+  },
+  {
+    id: 6,
+    name: "Pizza a la Leña Grande",
+    price: 6.5,
+  },
+  {
+    id: 7,
+    name: "Napolitana York Queso",
+    price: 2.1,
+  },
+  {
+    id: 8,
+    name: "Zumo de Limón",
+    price: 1.7,
+  },
+  {
+    id: 9,
+    name: "Café con leche",
+    price: 1.5,
+  },
+  {
+    id: 10,
+    name: "Rebanada de Pan con Jamón",
+    price: 2.5,
+  },
+  {
+    id: 11,
+    name: "Café Americano",
+    price: 1.5,
+  },
+  {
+    id: 12,
+    name: "Café Capuchino",
+    price: 2.0,
+  },
+];
